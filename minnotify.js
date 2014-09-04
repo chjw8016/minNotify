9a534248406210d09b5e7bb9da58634db566cc39
chjw <chjw8016@163.com>
2014-09-04 17:13:23 +0800

    11

--- minnotify.js
+++ minnotify.js
@@ -7,28 +7,45 @@
             content:"",
             escClose:false,
             closeBgClick:false,
-            btnClose:true,
-            confirm:true,
+            btnClose:false,
+            confirm:false,
+            autoClose:false,
+            hasTitle:false,
+            alignCenter:true,
+            millisec:2000,
             callBack:function(e){
                 return e;
             }
         }
-        var opts = $.extend(defaults, options);
+        var opts =  $.extend( defaults, options);
         if($(".cd-popup").length) {
-           $(".cd-popup-container").find("p").text(opts.content)
+            $(".cd-popup").remove()
         }
-        else {
-            var html ='<div class="cd-popup" role="alert"> '+
-                '<div class="cd-popup-container"> '+
-                '<p>' + opts.content +'</p>'+
-                '<a href="#" class="cd-popup-close img-replace">关闭</a>'+
-                '</div> '+
-                '</div>'
-            $("body").append(html)
-            $(".cd-popup-close").on("click",function(e){
 
-                    $('.cd-popup').removeClass('is-visible');
+        var html = '<div class="cd-popup" role="alert">' +
+                '<div class="cd-popup-container"> <div class="cd-popup-title">提示信息</div> ' +
+                '<p>' + opts.content + '</p>' +
+                '</div> ' +
+                '</div>'
+        $("body").append(html)
+        if (!opts.hasTitle) {
+            $(".cd-popup-title").remove()
+        }
+        if(opts.alignCenter) {
+            $(".cd-popup-container p").css("text-align","center");
+        }
+        if(!opts.autoClose&&!opts.confirm) {
+            var _btnClose = $('<a href="#" class="cd-popup-close img-replace">关闭</a>');
+            _btnClose.on("click", function (e) {
+                $('.cd-popup').removeClass('is-visible');
             })
+            $(".cd-popup-container").append(_btnClose);
+        }
+
+        if(opts.autoClose) {
+            setTimeout(function(){
+                $('.cd-popup').removeClass('is-visible');
+            },opts.millisec)
         }
 
         if(opts.closeBgClick)
@@ -41,7 +58,7 @@
             })
         }
 
-        if(confirm)
+        if(opts.confirm)
         {
             var _btn = $("<div class='cd-buttons'><a>确&nbsp;认</a><a>取&nbsp;消</a></div>");
             _btn.find("a").first().on("click",function(){
@@ -50,15 +67,15 @@
             });
             _btn.find("a").last().on("click",function(){
                 $('.cd-popup').removeClass('is-visible');
-                opts. callBack(false);
+                opts.callBack(false);
             })
             $(".cd-popup-container").append(_btn);
         }
-        if(opts.btnClose && !confirm) {
+        if(opts.btnClose && !opts.confirm) {
             var _btn = $("<div class='cd-buttons'><a>关&nbsp;闭</a></div>");
             _btn.find("a").on("click", function () {
                 $('.cd-popup').removeClass('is-visible');
-            })
+            }).css({"width":"100%","border-radius": "0 0 .25em .25em"})
             $(".cd-popup-container").append(_btn);
         }
         $('.cd-popup').addClass('is-visible');
